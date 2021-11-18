e97514e07837a96ab7af26f5ebd0ff7f46f01019
Liliana <lily.gallegos@hotmail.com>
2021-11-18 12:37:56 -0600

    circle icon -responsive details-

--- src/pages/NewFeedBack/NewFeedBack.jsx
+++ src/pages/NewFeedBack/NewFeedBack.jsx
@@ -6,6 +6,21 @@ import './styles/newFeedback.css';
 
 const NewFeedBack = () => {
 
+    const icon = () => (
+        <svg width="56" height="56" viewBox="0 0 56 56" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="28" cy="28" r="28" fill="url(#paint0_radial_0_1342)" />
+            <path d="M30.3425 36V30.1657H36.0295V25.8637H30.3425V20H25.7459V25.8637H20V30.1657H25.7459V36H30.3425Z" fill="white" />
+            <defs>
+                <radialGradient id="paint0_radial_0_1342" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(58.184 -5.81647) rotate(129.411) scale(93.4169)">
+                    <stop stop-color="#E84D70" />
+                    <stop offset="0.530886" stop-color="#A337F6" />
+                    <stop offset="1" stop-color="#28A7ED" />
+                </radialGradient>
+            </defs>
+        </svg>
+
+    )
+
     function validateInputs(value) {
         let error
         if (!value) {
@@ -23,7 +38,14 @@ const NewFeedBack = () => {
                     <ChevronLeftIcon color="#4661E6" />
                     <a>Go Back</a>
                 </div>
+                <div className="circle-icon">
+                        {icon()}
+                    </div>
+
+
                 <div className="feedback-form">
+
+                 
                     <h1>Create New Feedback</h1>
                     <br />
                     <Formik
