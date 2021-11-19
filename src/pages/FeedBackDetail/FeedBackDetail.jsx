5ec33832fb803fb49e67141d40b2bf5e16c081c4
Liliana <lily.gallegos@hotmail.com>
2021-11-19 00:02:17 -0600

    comment -props-

--- src/pages/FeedBackDetail/FeedBackDetail.jsx
+++ src/pages/FeedBackDetail/FeedBackDetail.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SuggestionsCard from "../Suggestions/components/suggestionsCard";
+import Comment from "./components/comment";
 import AddComment from "./components/addComment";
 import { ChevronLeftIcon } from "@chakra-ui/icons";
 
@@ -17,8 +18,9 @@ const FeedBackDetails = () => {
                         </div>
                         <button className="button-blue">Edit Feedback</button>
                     </div>
-                    <SuggestionsCard styles={{maxWidth:"720px"}}/>
-                    <AddComment/>
+                    <SuggestionsCard styles={{ maxWidth: "720px" }} />
+                    <Comment name="Elijah Moss" username="@hexagon.bestagon" comment="Also, please allow styles to be applied based on system preferences. I would love to be able to browse Frontend Mentor in the evening after my device’s dark mode turns on without the bright background it currently has." />
+                    <AddComment />
                 </div>
             </div>
         </>
---
+++ src/pages/FeedBackDetail/components/comment.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import '../styles/comment.css'
+
+const Comment = ({ name, username, comment }) => {
+    return (
+        <>
+            <div className="comment-container">
+                <div className="comment-image">
+                    <img src="https://expertphotography.b-cdn.net/wp-content/uploads/2020/08/social-media-profile-photos-3.jpg"></img>
+                </div>
+                <div className="comment-content">
+                    <div className="comment-top">
+                        <div className="comment-user">
+                            <h3 className="username-comment">{name}</h3>
+                            <p>{username}</p>
+                        </div>
+                        <h2 className="reply">Reply</h2>
+                    </div>
+                    <p className="comment-body">{comment}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Comment;
