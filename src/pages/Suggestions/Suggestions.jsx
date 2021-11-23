b5d25ebc2b05ebb7303520b8cd34bab18cf78b97
Liliana <lily.gallegos@hotmail.com>
2021-11-23 14:24:46 -0600

    clean code

--- src/pages/Suggestions/Suggestions.jsx
+++ src/pages/Suggestions/Suggestions.jsx
@@ -10,26 +10,6 @@ import './styles/suggestion.css';
 import { useSelector } from 'react-redux';
 import { motion } from "framer-motion";
 
-const sidebar = {
-    open: (height = 1000) => ({
-        clipPath: `circle(${height * 2 + 200}px at 40px 40px)`,
-        transition: {
-            type: "spring",
-            stiffness: 20,
-            restDelta: 2
-        }
-    }),
-    closed: {
-        clipPath: "circle(30px at 40px 40px)",
-        transition: {
-            delay: 0.5,
-            type: "spring",
-            stiffness: 400,
-            damping: 40
-        }
-    }
-};
-
 const Suggestion = () => {
     const [menuStatus, setMenuStatus] = useState(false); //menu toggle
 
